81ab7ea97879e50cda74ab56c7f2d5e2d2d90aac
Hafiz Usman <hafizusmanismail2@gmail.com>
2023-08-19 01:37:11 +0500

    configured bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import './assets/main.css'
 
+// Import our custom CSS
+import './assets/styles.scss'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { dirname } from 'node:path' // Import dirname function
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+const __filename = fileURLToPath(import.meta.url) // Convert import.meta.url to __filename
+const __dirname = dirname(__filename) // Get the directory name from the filename
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~bootstrap': `${__dirname}/node_modules/bootstrap` // Use __dirname directly
     }
   }
 })
