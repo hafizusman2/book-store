04ec8f8985cc64c31990c4ca733ebf262a76eae3
Hafiz Usman <hafizusmanismail2@gmail.com>
2023-08-19 01:53:48 +0500

    added quasar ui library

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import './assets/main.css'
 
 // Import our custom CSS
 import './assets/styles.scss'
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
 
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -12,5 +18,11 @@ const app = createApp(App)
 
 app.use(router)
 app.use(store)
+app.use(Quasar, {
+  config: {
+    dark: true
+  },
+  plugins: {}
+})
 
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,20 @@ import { dirname } from 'node:path' // Import dirname function
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { quasar } from '@quasar/vite-plugin'
 
 const __filename = fileURLToPath(import.meta.url) // Convert import.meta.url to __filename
 const __dirname = dirname(__filename) // Get the directory name from the filename
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    quasar({
+      sassVariables: 'src/quasar-variables.sass'
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
