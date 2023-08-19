dc7ab6168950e49d3f61e55812dfcdff06f3aa84
Hafiz Usman <hafizusmanismail2@gmail.com>
2023-08-19 19:39:00 +0500

    add table - to view books data

--- src/views/BooksView.vue
+++ src/views/BooksView.vue
@@ -7,6 +7,43 @@
       Edit Book
     </router-link>
   </div>
+  <div class="q-pa-md">
+    <q-table title="Books" :rows="rows" :columns="columns" row-key="" />
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+import { getUserBooks } from '../services/book.js'
+const formatDate = (dateString) => {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' }
+  return new Date(dateString).toLocaleDateString(undefined, options)
+}
+const rows = ref([]) // Use ref to create a reactive variable
+
+onMounted(() => {
+  const bookData = getUserBooks(1)
+  if (!bookData) return
+  rows.value = bookData
+})
+const columns = [
+  {
+    name: 'id',
+    required: true,
+    label: 'Id',
+    align: 'right',
+    field: 'id',
+    sortable: true
+  },
+  { name: 'title', align: 'center', label: 'Title', field: 'title', sortable: true },
+  {
+    name: 'due_date',
+    label: 'Due Date',
+    field: 'due_date',
+    align: 'center',
+    sortable: true,
+    format: (val) => formatDate(val) // Format Date
+  },
+  { name: 'status', align: 'center', label: 'Status', field: 'status', sortable: true }
+]
+</script>
