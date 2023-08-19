63e1f7513b234c3cf3c97f90dfb3f99f23684df4
Hafiz Usman <hafizusmanismail2@gmail.com>
2023-08-19 22:15:41 +0500

    firebase config

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY || 'AIzaSyA8APb_6Vbc8PjfJ9t1gsAy4HxZ5c7iS40',
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN || 'react-firebase-auth-790f9.firebaseapp.com',
+  projectId: import.meta.env.VITE_PROJECT_ID || 'react-firebase-auth-790f9',
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET || 'react-firebase-auth-790f9.appspot.com',
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID || '962127514066',
+  appId: import.meta.env.VITE_APP_ID || '1:962127514066:web:52b964fe84be83f204a00b',
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID || 'G-V73GJVVH1W'
+}
